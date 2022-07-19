4f5333ebe77073dd9f0c2f76d8506bc069e1f060
Denis Talochkin <denis.talochkin@gmail.com>
2022-07-19 19:25:32 +0300

    Disable console output in production mode

--- src/App.js
+++ src/App.js
@@ -5,6 +5,14 @@ import ClipBoard from "./ClipBoard";
 
 Amplify.configure(awsExports);
 
+if (process.env.REACT_APP_ENV === 'production') {
+  global.consoleDebug = console.debug;
+  console.debug = function () {
+    global.consoleDebug("Console debug is disabled in production");
+  }
+}
+
+
 function App() {
   return (
     <ClipBoard />
